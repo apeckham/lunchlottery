84fd2f8214cca59d9903cd2ab915ce9b5b10b300
Heather Moore <moore.hj@gmail.com>
2013-05-15 18:57:48 -0700

    mod day calculation for end of week lunches

--- lib/lottery.rb
+++ lib/lottery.rb
@@ -1,6 +1,6 @@
 module Lottery
   def self.send_invitations!
-    Location.where(day: Date.today.wday + 2).each do |location|
+    Location.where(day: days_out(2)).each do |location|
       shuffled_people = location.people.subscribed.opted_in.all.shuffle
 
       if shuffled_people.length > 2
@@ -15,9 +15,14 @@ module Lottery
 
   def self.send_reminders!
     Person.subscribed.each do |person|
-      if person.location.day == Date.today.wday + 1
+      if person.location.day == days_out(1)
         Notifier.remind(person).deliver
       end
     end
+  end
+
+  def self.days_out num
+    zero_based_days_in_week = 6
+    (Date.today.wday + num) % zero_based_days_in_week
   end
 end
