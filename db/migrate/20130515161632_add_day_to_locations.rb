816c6909ff38db2624f60c8ce6c6a2abd5517fcf
Heather Moore <moore.hj@gmail.com>
2013-05-15 19:24:40 -0700

    actually remove comment in migration about date spec

--- db/migrate/20130515161632_add_day_to_locations.rb
+++ db/migrate/20130515161632_add_day_to_locations.rb
@@ -1,6 +1,6 @@
 class AddDayToLocations < ActiveRecord::Migration
   def self.up
-    #  ISO-8601 standard, 0 is Sunday, 6 is Saturday
+    #  0 is Sunday, 6 is Saturday
     add_column :locations, :day, :integer, default: 2
   end
 
